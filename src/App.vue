e35b994760a67d986af41dffd24521b567c2b16f
jarji <100996174+jarjia@users.noreply.github.com>
2024-01-29 22:37:29 +0400

    fe

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,12 @@ let pc = null;
 
     pc = new RTCPeerConnection({
       iceServers: [
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
         {
           urls: "turn:openrelay.metered.ca:80",
           username: "openrelayproject",
-          credentials: "openrelayproject",
+          credential: "openrelayproject",
         },
       ],
     });
